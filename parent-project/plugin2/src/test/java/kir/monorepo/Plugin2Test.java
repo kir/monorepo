8578772af070915aa4943522955434ecb66ea68e
Kirill (KIR) Maximov <kir@maxkir.com>
2024-10-03 17:49:04 +0200

    A change in Plugin2

--- parent-project/plugin2/src/test/java/kir/monorepo/Plugin2Test.java
+++ parent-project/plugin2/src/test/java/kir/monorepo/Plugin2Test.java
@@ -15,6 +15,6 @@ public class Plugin2Test {
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(false);
-        // a change in plugin 4
+        // a change in plugin 5
     }
 }
